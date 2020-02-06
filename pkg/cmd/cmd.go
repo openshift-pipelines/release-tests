1c43e5ff52e02efb1987af7aaae66523eae18dfe
Sunil Thaha <sthaha@redhat.com>
2020-02-06 18:44:58 +1000

    Fix cmd.AssertOutput so that step fails on assertion failure

--- pkg/cmd/cmd.go
+++ pkg/cmd/cmd.go
@@ -1,15 +1,35 @@
 package cmd
 
 import (
-	"testing"
+	"fmt"
 
+	"github.com/getgauge-contrib/gauge-go/testsuit"
 	"github.com/openshift-pipelines/release-tests/pkg/config"
+	"gotest.tools/assert"
 	"gotest.tools/v3/icmd"
 )
 
 type Cmd struct {
 	Args     []string
 	Expected icmd.Expected
+}
+
+// testsuitAdaptor bridges the gap between testsuit.T and assert.TestingT
+type testsuitAdaptor struct{}
+
+// ensure testsuitAdaptor satisfies assert.TestingT interface
+var _ assert.TestingT = (*testsuitAdaptor)(nil)
+
+func (ta testsuitAdaptor) Fail() {
+	testsuit.T.Fail(fmt.Errorf("Step failed execute"))
+}
+
+func (ta testsuitAdaptor) FailNow() {
+	testsuit.T.Fail(fmt.Errorf("Step failed to execute"))
+}
+
+func (ta testsuitAdaptor) Log(args ...interface{}) {
+	testsuit.T.Fail(fmt.Errorf("%v", args))
 }
 
 func Run(cmd []string) *icmd.Result {
@@ -20,8 +40,7 @@ func Run(cmd []string) *icmd.Result {
 func AssertOutput(cmd *Cmd) *icmd.Result {
 	res := Run(cmd.Args)
 
-	// TODO: fix this hack
-	var t = &testing.T{}
+	t := &testsuitAdaptor{}
 	res.Assert(t, cmd.Expected)
 	return res
 }
