7dec5e7f30c936bddd48b65c75e975eaac921d30
Pavol Pitonak <ppitonak@redhat.com>
2022-08-02 15:36:44 +0200

    restore pruner config after spec

--- steps/hooks.go
+++ steps/hooks.go
@@ -1,10 +1,17 @@
 package steps
 
 import (
+	"context"
+	"fmt"
+	"strconv"
+	"strings"
+
 	"github.com/getgauge-contrib/gauge-go/gauge"
 	"github.com/getgauge-contrib/gauge-go/testsuit"
 	"github.com/openshift-pipelines/release-tests/pkg/config"
 	"github.com/openshift-pipelines/release-tests/pkg/k8s"
+	"github.com/openshift-pipelines/release-tests/pkg/oc"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 )
 
 // Runs Before every Secenario
@@ -36,3 +43,40 @@ var _ = gauge.AfterScenario(func() {
 		testsuit.T.Errorf("Error: return type is not of type func()")
 	}
 }, []string{}, testsuit.AND)
+
+// Store default pruner config
+var _ = gauge.BeforeSpec(func() {
+	cs, _, _ := k8s.NewClientSet()
+
+	tc, err := cs.TektonConfig().Get(context.TODO(), config.TektonConfigName, metav1.GetOptions{})
+	if err != nil {
+		testsuit.T.Errorf("Error: could not get TektonConfig")
+	}
+
+	store := gauge.GetSpecStore()
+	if tc.Spec.Pruner.Keep != nil {
+		store["keep"] = strconv.FormatUint(uint64(*(tc.Spec.Pruner.Keep)), 10)
+	} else {
+		store["keep"] = "null"
+	}
+	if tc.Spec.Pruner.KeepSince != nil {
+		store["keepSince"] = strconv.FormatUint(uint64(*(tc.Spec.Pruner.KeepSince)), 10)
+	} else {
+		store["keepSince"] = "null"
+	}
+	store["resources"] = tc.Spec.Pruner.Resources
+	store["schedule"] = tc.Spec.Pruner.Schedule
+}, []string{"auto-prune"}, testsuit.AND)
+
+// Revert changes made by pruner tests
+var _ = gauge.AfterSpec(func() {
+	keep := gauge.GetSpecStore()["keep"]
+	keepSince := gauge.GetSpecStore()["keepSince"]
+	resources := gauge.GetSpecStore()["resources"].([]string)
+	schedule := gauge.GetSpecStore()["schedule"]
+
+	resourcesList := strings.Join(resources, "\",\"")
+	patch_data := fmt.Sprintf("{\"spec\":{\"pruner\":{\"keep\":%s,\"keep-since\":%s,\"schedule\":\"%s\",\"resources\":[\"%s\"]}}}", keep, keepSince, schedule, resourcesList)
+
+	oc.UpdateTektonConfig(patch_data)
+}, []string{"auto-prune"}, testsuit.AND)
