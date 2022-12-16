e8dd316f7957d21b8d599f8961a22b9ae465a43e
Pavol Pitonak <ppitonak@redhat.com>
2022-12-16 13:42:43 +0100

    check that image stream golang exists before using it

--- pkg/openshift/openshift.go
+++ pkg/openshift/openshift.go
@@ -4,9 +4,13 @@ import (
 	"fmt"
 	"log"
 
+	"github.com/getgauge-contrib/gauge-go/testsuit"
 	"github.com/openshift-pipelines/release-tests/pkg/clients"
+	"github.com/openshift-pipelines/release-tests/pkg/config"
 	imageStream "github.com/openshift/client-go/image/clientset/versioned"
+	"k8s.io/apimachinery/pkg/api/errors"
 	v1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	"k8s.io/apimachinery/pkg/util/wait"
 )
 
 func GetImageStreamTags(c *clients.Clients, namespace, name string) []string {
@@ -22,4 +26,19 @@ func GetImageStreamTags(c *clients.Clients, namespace, name string) []string {
 		tagNames = append(tagNames, tag.Name)
 	}
 	return tagNames
+}
+
+func VerifyImageStreamExists(c *clients.Clients, name, namespace string) {
+	log.Printf("Verify that image stream %q exists in namespace %q", name, namespace)
+	is := imageStream.NewForConfigOrDie(c.KubeConfig)
+
+	if err := wait.PollImmediate(config.APIRetry, config.APITimeout, func() (bool, error) {
+		_, err := is.ImageV1().ImageStreams(namespace).Get(c.Ctx, name, v1.GetOptions{})
+		if err != nil && errors.IsNotFound(err) {
+			return false, nil
+		}
+		return true, err
+	}); err != nil {
+		testsuit.T.Errorf("Failed to get image stream %q in namespace %q for tests: %s", name, namespace, err)
+	}
 }
