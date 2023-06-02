9f18eb8e246014c432256c2484f8d68f4b8e5dbe
Veeresh Aradhya <varadhya@redhat.com>
2023-06-02 15:47:31 +0530

    Fixing pruner tests

--- steps/cli/oc.go
+++ steps/cli/oc.go
@@ -44,13 +44,13 @@ var _ = gauge.Step("Update pruner config <keepPresence> keep <keep> schedule <sc
 	resourcesList := strings.Join(resourcesSplit, "\",\"")
 	patch_data := ""
 	if keepPresence == "with" && keepSincePresence == "without" {
-		patch_data = fmt.Sprintf("{\"spec\":{\"pruner\":{\"keep\":%s,\"schedule\":\"%s\",\"resources\":[\"%s\"]}}}", keep, schedule, resourcesList)
+		patch_data = fmt.Sprintf("{\"spec\":{\"pruner\":{\"keep\":%s,\"keep-since\":null,\"schedule\":\"%s\",\"resources\":[\"%s\"]}}}", keep, schedule, resourcesList)
 	} else if keepPresence == "without" && keepSincePresence == "with" {
-		patch_data = fmt.Sprintf("{\"spec\":{\"pruner\":{\"keep-since\":%s,\"schedule\":\"%s\",\"resources\":[\"%s\"]}}}", keepSince, schedule, resourcesList)
+		patch_data = fmt.Sprintf("{\"spec\":{\"pruner\":{\"keep\":null,\"keep-since\":%s,\"schedule\":\"%s\",\"resources\":[\"%s\"]}}}", keepSince, schedule, resourcesList)
 	} else if keepPresence == "with" && keepSincePresence == "with" {
 		patch_data = fmt.Sprintf("{\"spec\":{\"pruner\":{\"keep\":%s,\"keep-since\":%s,\"schedule\":\"%s\",\"resources\":[\"%s\"]}}}", keep, keepSince, schedule, resourcesList)
 	} else if keepPresence == "without" && keepSincePresence == "without" {
-		patch_data = fmt.Sprintf("{\"spec\":{\"pruner\":{\"schedule\":\"%s\",\"resources\":[\"%s\"]}}}", schedule, resourcesList)
+		patch_data = fmt.Sprintf("{\"spec\":{\"pruner\":{\"keep\":null,\"keep-since\":null,\"schedule\":\"%s\",\"resources\":[\"%s\"]}}}", schedule, resourcesList)
 	}
 	oc.UpdateTektonConfig(patch_data)
 })
@@ -60,13 +60,13 @@ var _ = gauge.Step("Update pruner config with invalid data <keepPresence> keep <
 	resourcesList := strings.Join(resourcesSplit, "\",\"")
 	patch_data := ""
 	if keepPresence == "with" && keepSincePresence == "without" {
-		patch_data = fmt.Sprintf("{\"spec\":{\"pruner\":{\"keep\":%s,\"schedule\":\"%s\",\"resources\":[\"%s\"]}}}", keep, schedule, resourcesList)
+		patch_data = fmt.Sprintf("{\"spec\":{\"pruner\":{\"keep\":%s,\"keep-since\":null,\"schedule\":\"%s\",\"resources\":[\"%s\"]}}}", keep, schedule, resourcesList)
 	} else if keepPresence == "without" && keepSincePresence == "with" {
-		patch_data = fmt.Sprintf("{\"spec\":{\"pruner\":{\"keep-since\":%s,\"schedule\":\"%s\",\"resources\":[\"%s\"]}}}", keepSince, schedule, resourcesList)
+		patch_data = fmt.Sprintf("{\"spec\":{\"pruner\":{\"keep\":null,\"keep-since\":%s,\"schedule\":\"%s\",\"resources\":[\"%s\"]}}}", keepSince, schedule, resourcesList)
 	} else if keepPresence == "with" && keepSincePresence == "with" {
 		patch_data = fmt.Sprintf("{\"spec\":{\"pruner\":{\"keep\":%s,\"keep-since\":%s,\"schedule\":\"%s\",\"resources\":[\"%s\"]}}}", keep, keepSince, schedule, resourcesList)
 	} else if keepPresence == "without" && keepSincePresence == "without" {
-		patch_data = fmt.Sprintf("{\"spec\":{\"pruner\":{\"schedule\":\"%s\",\"resources\":[\"%s\"]}}}", schedule, resourcesList)
+		patch_data = fmt.Sprintf("{\"spec\":{\"pruner\":{\"keep\":null,\"keep-since\":null,\"schedule\":\"%s\",\"resources\":[\"%s\"]}}}", schedule, resourcesList)
 	}
 	oc.UpdateTektonConfigwithInvalidData(patch_data, errorMessage)
 })
