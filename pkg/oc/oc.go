eeb9550cf29a2e9f19e0ac20d0bcc02786d4ceac
amisskii <124462506+amisskii@users.noreply.github.com>
2023-06-08 13:49:35 +0200

    Add Apply step (#146)

--- pkg/oc/oc.go
+++ pkg/oc/oc.go
@@ -16,6 +16,10 @@ func Create(path_dir, namespace string) {
 	log.Printf("output: %s\n", cmd.MustSucceed("oc", "create", "-f", resource.Path(path_dir), "-n", namespace).Stdout())
 }
 
+func Apply(path_dir, namespace string){
+	log.Printf("output: %s\n", cmd.MustSucceed("oc", "apply", "-f", resource.Path(path_dir), "-n", namespace).Stdout())
+}
+
 // Delete resources using oc command
 func Delete(path_dir, namespace string) {
 	log.Printf("output: %s\n", cmd.MustSucceed("oc", "delete", "-f", resource.Path(path_dir), "-n", namespace).Stdout())
