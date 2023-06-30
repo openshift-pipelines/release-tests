645482ba79fb353ccd0c8addab7808bf3e4f7aec
amisskii <124462506+amisskii@users.noreply.github.com>
2023-06-30 10:57:08 +0200

    check if secret exists (#159)

--- steps/cli/oc.go
+++ steps/cli/oc.go
@@ -8,7 +8,6 @@ import (
 
 	"github.com/getgauge-contrib/gauge-go/gauge"
 	m "github.com/getgauge-contrib/gauge-go/models"
-	"github.com/getgauge-contrib/gauge-go/testsuit"
 	"github.com/openshift-pipelines/release-tests/pkg/oc"
 	"github.com/openshift-pipelines/release-tests/pkg/store"
 )
@@ -146,10 +145,14 @@ var _ = gauge.Step("Change enable-api-fields to <version>", func(version string)
 
 var _ = gauge.Step("Configure GitHub token for git resolver in TektonConfig", func() {
 	if os.Getenv("GITHUB_TOKEN") == "" {
-		testsuit.T.Errorf("Token for authorization to the GitHub repository was not exported as a system variable")
+		log.Printf("Token for authorization to the GitHub repository was not exported as a system variable")
 	} else {
-		secretData := os.Getenv("GITHUB_TOKEN")
-		oc.CreateSecretForGitResolver(secretData, store.Namespace())
+		if !oc.SecretExists("github-auth-secret", store.Namespace()){
+			secretData := os.Getenv("GITHUB_TOKEN")
+			oc.CreateSecretForGitResolver(secretData, store.Namespace())
+		} else {
+			log.Printf("Secret \"github-auth-secret\" already exists")
+		}
 		patch_data := fmt.Sprintf("{\"spec\":{\"pipeline\":{\"git-resolver-config\":{\"api-token-secret-key\":\"github-auth-key\", \"api-token-secret-name\":\"github-auth-secret\", \"api-token-secret-namespace\":\"openshift-pipelines\", \"default-revision\":\"main\", \"fetch-timeout\":\"1m\", \"scm-type\":\"github\"}}}}")
 		oc.UpdateTektonConfig(patch_data)
 	}
