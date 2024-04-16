c792e348847d07c221b402788fc4617933771e10
Veeresh Aradhya <varadhya@redhat.com>
2024-04-16 15:28:26 +0530

    Adding pipelines tutorials tests (#238)

--- pkg/oc/oc.go
+++ pkg/oc/oc.go
@@ -18,6 +18,11 @@ func Create(path_dir, namespace string) {
 	log.Printf("output: %s\n", cmd.MustSucceed("oc", "create", "-f", resource.Path(path_dir), "-n", namespace).Stdout())
 }
 
+// Create resources using remote path using oc command
+func CreateRemote(remote_path, namespace string) {
+	log.Printf("output: %s\n", cmd.MustSucceed("oc", "create", "-f", remote_path, "-n", namespace).Stdout())
+}
+
 func Apply(path_dir, namespace string) {
 	log.Printf("output: %s\n", cmd.MustSucceed("oc", "apply", "-f", resource.Path(path_dir), "-n", namespace).Stdout())
 }
