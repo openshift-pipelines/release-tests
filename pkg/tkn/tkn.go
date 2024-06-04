0e321153a3a0d2efa147915390f65309803a8648
Sri Vignesh <sselvan@redhat.com>
2024-06-04 17:02:22 +0530

    correct resource name
    Signed-off-by: Sri Vignesh <sselvan@redhat.com>

--- pkg/tkn/tkn.go
+++ pkg/tkn/tkn.go
@@ -42,7 +42,7 @@ func AssertComponentVersion(version string, component string) {
 	case "results":
 		actualVersion = cmd.MustSucceed("oc", "get", "tektonresult", "result", "-o", "jsonpath={.status.version}").Stdout()
 	case "manual-approval-gate":
-		actualVersion = cmd.MustSucceed("oc", "get", "manualapprovalgate", "manualapprovalgate", "-o", "jsonpath={.status.version}").Stdout()
+		actualVersion = cmd.MustSucceed("oc", "get", "manualapprovalgate", "manual-approval-gate", "-o", "jsonpath={.status.version}").Stdout()
 	default:
 		testsuit.T.Errorf("Unknown component")
 	}
