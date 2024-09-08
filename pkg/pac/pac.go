7e2b83ed9635434958500e68a39a332e8ac651aa
Sri Vignesh <sselvan@redhat.com>
2024-09-08 17:01:35 +0530

    reorder functions
    Signed-off-by: Sri Vignesh <sselvan@redhat.com>

--- pkg/pac/pac.go
+++ pkg/pac/pac.go
@@ -5,20 +5,29 @@ import (
 	"fmt"
 	"io"
 	"log"
-	"net/url"
+	"net/http"
+	"os"
 	"os/exec"
 	"strconv"
 	"strings"
 	"time"
 
+	"github.com/getgauge-contrib/gauge-go/testsuit"
 	"github.com/openshift-pipelines/release-tests/pkg/clients"
+	"github.com/openshift-pipelines/release-tests/pkg/cmd"
+	"github.com/openshift-pipelines/release-tests/pkg/config"
+	"github.com/openshift-pipelines/release-tests/pkg/k8s"
+	"github.com/openshift-pipelines/release-tests/pkg/store"
+	"github.com/openshift-pipelines/release-tests/pkg/triggers"
+	"github.com/openshift-pipelines/release-tests/pkg/wait"
+	eventReconciler "github.com/tektoncd/triggers/pkg/reconciler/eventlistener"
 	"github.com/xanzy/go-gitlab"
 	v1 "k8s.io/api/apps/v1"
 	corev1 "k8s.io/api/core/v1"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 )
 
-func GetNewSmeeURL() (string, error) {
+func getNewSmeeURL() (string, error) {
 	curlCommand := `curl -Ls -o /dev/null -w %{url_effective} https://smee.io/new`
 
 	cmd := exec.Command("sh", "-c", curlCommand)
@@ -36,7 +45,7 @@ func GetNewSmeeURL() (string, error) {
 	return smeeURL, nil
 }
 
-func CreateSmeeDeployment(c *clients.Clients, namespace, smeeURL, targetURL string) error {
+func createSmeeDeployment(c *clients.Clients, namespace, smeeURL, targetURL string) error {
 	deployment := &v1.Deployment{
 		ObjectMeta: metav1.ObjectMeta{
 			Name: "gosmee-client",
@@ -100,39 +109,43 @@ func CreateSmeeDeployment(c *clients.Clients, namespace, smeeURL, targetURL stri
 
 func int32Ptr(i int32) *int32 { return &i }
 
-func InitGitLabClient(privateToken string) (*gitlab.Client, error) {
-	client, err := gitlab.NewClient(privateToken)
-	if err != nil {
-		return nil, fmt.Errorf("failed to create GitLab client: %w", err)
-	}
-	return client, nil
-}
+func forkProject(client *gitlab.Client, projectID, targetGroupNamespace string) (*gitlab.Project, error) {
+	var project *gitlab.Project
+	var response *gitlab.Response
+	var err error
+	maxRetries := 5
+	for i := 0; i < maxRetries; i++ {
+		randomSuffix := strconv.FormatInt(time.Now().UnixNano(), 10)[:8]
+		projectName := "openshift-pipelines-test-fork-" + randomSuffix
 
-func ForkProject(client *gitlab.Client, projectID, targetGroupNamespace string) (*gitlab.Project, error) {
-	randomSuffix := strconv.FormatInt(time.Now().UnixNano(), 10)[:8]
-	projectName := "openshift-pipelines-test-fork-" + randomSuffix
-	projectPath := "openshift-pipelines-test-fork-" + randomSuffix
-
-	forkOptions := &gitlab.ForkProjectOptions{
-		Namespace: &targetGroupNamespace,
-		Name:      &projectName,
-		Path:      &projectPath,
-	}
-
-	project, response, err := client.Projects.ForkProject(projectID, forkOptions)
-	if err != nil {
-		log.Printf("Failed to fork project: %v\n", err)
-		if response != nil {
-			if body, readErr := io.ReadAll(response.Body); readErr == nil {
-				log.Printf("Response body: %s\n", string(body))
-			}
+		forkOptions := &gitlab.ForkProjectOptions{
+			Namespace: &targetGroupNamespace,
+			Name:      &projectName,
+			Path:      &projectName,
 		}
-		return nil, err
+
+		project, response, err = client.Projects.ForkProject(projectID, forkOptions)
+		if err != nil {
+			log.Printf("Attempt %d: Failed to fork project: %v\n", i+1, err)
+			if response != nil {
+				if body, readErr := io.ReadAll(response.Body); readErr == nil {
+					log.Printf("Response body: %s\n", string(body))
+				}
+				if response.StatusCode == http.StatusConflict {
+					log.Printf("Project name or path already exists, trying again with a new name.")
+					continue
+				}
+			}
+			return nil, err
+		}
+
+		return project, nil
 	}
-	return project, nil
+
+	return nil, fmt.Errorf("failed to fork project after %d attempts", maxRetries)
 }
 
-func AddWebhook(client *gitlab.Client, projectID int, webhookURL, token string) error {
+func addWebhook(client *gitlab.Client, projectID int, webhookURL, token string) error {
 	pushEvents := true
 	mergeRequestsEvents := true
 	hookOptions := &gitlab.AddProjectHookOptions{
@@ -148,7 +161,7 @@ func AddWebhook(client *gitlab.Client, projectID int, webhookURL, token string) 
 	return nil
 }
 
-func CreateBranch(client *gitlab.Client, projectID int, branchName string) error {
+func createBranch(client *gitlab.Client, projectID int, branchName string) error {
 	_, _, err := client.Branches.CreateBranch(projectID, &gitlab.CreateBranchOptions{
 		Branch: gitlab.Ptr(branchName),
 		Ref:    gitlab.Ptr("main"),
@@ -156,7 +169,7 @@ func CreateBranch(client *gitlab.Client, projectID int, branchName string) error
 	return err
 }
 
-func CreateCommit(client *gitlab.Client, projectID int, branchName string, commitMessage string) error {
+func createCommit(client *gitlab.Client, projectID int, branchName string, commitMessage string) error {
 	actionValue := gitlab.FileActionValue("create")
 	filePath := "preview.md"
 	content := "Preview changes for the new feature"
@@ -175,7 +188,7 @@ func CreateCommit(client *gitlab.Client, projectID int, branchName string, commi
 	return err
 }
 
-func CreateMergeRequest(client *gitlab.Client, projectID int, sourceBranch, targetBranch, title string) (string, error) {
+func createMergeRequest(client *gitlab.Client, projectID int, sourceBranch, targetBranch, title string) (string, error) {
 	mrOptions := &gitlab.CreateMergeRequestOptions{
 		SourceBranch: &sourceBranch,
 		TargetBranch: &targetBranch,
@@ -188,68 +201,139 @@ func CreateMergeRequest(client *gitlab.Client, projectID int, sourceBranch, targ
 	return mr.WebURL, nil
 }
 
-func ExtractMergeRequestID(mrURL string) (int, error) {
-	parsedURL, err := url.Parse(mrURL)
-	if err != nil {
-		return 0, fmt.Errorf("failed to parse merge request URL: %w", err)
-	}
-	segments := strings.Split(parsedURL.Path, "/")
-	mrIDStr := segments[len(segments)-1]
-	mrID, err := strconv.Atoi(mrIDStr)
-	if err != nil {
-		return 0, fmt.Errorf("failed to convert MR ID to integer: %w", err)
-	}
-	return mrID, nil
-}
-
-func isTerminalStatus(status string) bool {
-	switch status {
-	case "success", "failed", "canceled":
-		return true
-	default:
-		return false
-	}
-}
-
-func CheckPipelineStatus(client *gitlab.Client, projectID int, mergeRequestID int) error {
-
-	const maxRetries = 10
-	var retryCount int
-
-	for {
-		pipelines, _, err := client.MergeRequests.ListMergeRequestPipelines(projectID, mergeRequestID)
-		if err != nil {
-			return fmt.Errorf("failed to list merge request pipelines: %w", err)
-		}
-
-		if len(pipelines) == 0 {
-			if retryCount >= maxRetries {
-				log.Printf("No pipelines found for the MR id %d after %d retries\n", mergeRequestID, maxRetries)
-				return nil
-			}
-			log.Println("No pipelines found, retrying...")
-			retryCount++
-			time.Sleep(time.Duration(2^retryCount) * time.Second)
-			continue
-		}
-
-		latestPipeline := pipelines[0]
-
-		if isTerminalStatus(latestPipeline.Status) {
-			log.Printf("Latest pipeline status for MR #%d: %s\n", mergeRequestID, latestPipeline.Status)
-			return nil
-		} else {
-			log.Println("waiting for Pipeline status to be updated...")
-			time.Sleep(30 * time.Second)
-		}
-	}
-}
-
-func DeleteGitlabProject(client *gitlab.Client, projectID int) error {
+func deleteGitlabProject(client *gitlab.Client, projectID int) error {
 	_, err := client.Projects.DeleteProject(projectID)
 	if err != nil {
 		return fmt.Errorf("failed to delete project: %w", err)
 	}
 	log.Println("Project successfully deleted.")
 	return nil
+}
+
+func InitGitLabClient(privateToken string) *gitlab.Client {
+	client, err := gitlab.NewClient(privateToken)
+	if err != nil {
+		testsuit.T.Fail(fmt.Errorf("failed to create GitLab client: %w", err))
+	}
+	return client
+}
+
+func SmeeDeployment(elname string) {
+	var err error
+	smeeDeploymentName := "gosmee-client"
+	store.PutScenarioData("smee_deployment_name", smeeDeploymentName)
+
+	smeeURL, err := getNewSmeeURL()
+	if err != nil {
+		testsuit.T.Fail(fmt.Errorf("Failed to get a new Smee URL: %v", err))
+	}
+	store.PutScenarioData("SMEE_URL", smeeURL)
+
+	routeurl := triggers.GetRoute(elname, store.Namespace())
+	store.PutScenarioData("route", routeurl)
+	store.PutScenarioData("elname", elname)
+
+	if err = createSmeeDeployment(store.Clients(), store.Namespace(), smeeURL, routeurl); err != nil {
+		testsuit.T.Fail(fmt.Errorf("Failed to create deployment: %v", err))
+	}
+}
+
+func SetupGitLabProject(client *gitlab.Client) *gitlab.Project {
+
+	gitlabGroupNamespace := config.GitlabGroupNamespace
+	projectIDOrPath := config.ProjectID
+	smeeURL := store.GetScenarioData("SMEE_URL")
+	token := config.TriggersSecretToken
+
+	project, err := forkProject(client, projectIDOrPath, gitlabGroupNamespace)
+	if err != nil {
+		testsuit.T.Fail(fmt.Errorf("error during project forking: %w", err))
+	}
+
+	defer func() {
+		if err != nil {
+			if cleanupErr := deleteGitlabProject(client, project.ID); cleanupErr != nil {
+				testsuit.T.Fail(fmt.Errorf("cleanup failed: %v", cleanupErr))
+			}
+		}
+	}()
+
+	err = addWebhook(client, project.ID, smeeURL, token)
+	if err != nil {
+		testsuit.T.Fail(fmt.Errorf("failed to add webhook: %w", err))
+	}
+
+	return project
+}
+
+func ConfigurePreviewChanges(client *gitlab.Client, projectID int) {
+
+	randomSuffix := strconv.FormatInt(time.Now().UnixNano(), 10)[:8]
+	branchName := "preview-branch-" + randomSuffix
+	commitMessage := "Add preview changes for feature"
+
+	if err := createBranch(client, projectID, branchName); err != nil {
+		testsuit.T.Fail(fmt.Errorf("Failed to create branch: %v", err))
+	}
+
+	if err := createCommit(client, projectID, branchName, commitMessage); err != nil {
+		testsuit.T.Fail(fmt.Errorf("Failed to create commit: %v", err))
+	}
+
+	mrURL, err := createMergeRequest(client, projectID, branchName, "main", "Add preview changes for feature")
+	if err != nil {
+		testsuit.T.Fail(fmt.Errorf("Failed to create merge request: %v", err))
+	}
+
+	log.Printf("Merge Request Created: %s\n", mrURL)
+}
+
+func CleanupPAC(c *clients.Clients, elName, smeeDeploymentName, namespace string) {
+	// Delete EventListener
+	err := c.TriggersClient.TriggersV1alpha1().EventListeners(namespace).Delete(c.Ctx, elName, metav1.DeleteOptions{})
+	if err != nil {
+		testsuit.T.Fail(err)
+	}
+
+	log.Println("Deleted EventListener")
+
+	// Verify the EventListener's Deployment is deleted
+	err = wait.WaitFor(c.Ctx, wait.DeploymentNotExist(c, namespace, fmt.Sprintf("%s-%s", eventReconciler.GeneratedResourcePrefix, elName)))
+	if err != nil {
+		testsuit.T.Fail(err)
+	}
+
+	log.Println("EventListener's Deployment was deleted")
+
+	// Verify the EventListener's Service is deleted
+	err = wait.WaitFor(c.Ctx, wait.ServiceNotExist(c, namespace, fmt.Sprintf("%s-%s", eventReconciler.GeneratedResourcePrefix, elName)))
+	if err != nil {
+		testsuit.T.Fail(err)
+	}
+
+	log.Println("EventListener's Service was deleted")
+
+	// Delete Route exposed earlier
+	err = c.Route.Routes(namespace).Delete(c.Ctx, fmt.Sprintf("%s-%s", eventReconciler.GeneratedResourcePrefix, elName), metav1.DeleteOptions{})
+	if err != nil {
+		testsuit.T.Fail(err)
+	}
+
+	// Verify the EventListener's Route is deleted
+	err = wait.WaitFor(c.Ctx, wait.RouteNotExist(c, namespace, fmt.Sprintf("%s-%s", eventReconciler.GeneratedResourcePrefix, elName)))
+	if err != nil {
+		testsuit.T.Fail(err)
+	}
+	log.Println("EventListener's Route got deleted successfully...")
+
+	// Delete Smee Deployment
+	err = k8s.DeleteDeployment(c, namespace, smeeDeploymentName)
+	if err != nil {
+		testsuit.T.Fail(err)
+	}
+
+	log.Println("Deleted Smee Deployment")
+
+	// This is required when EL runs as TLS
+	cmd.MustSucceed("rm", "-rf", os.Getenv("GOPATH")+"/src/github.com/openshift-pipelines/release-tests/testdata/triggers/certs")
 }
