1682f84169f861494a6d731f8d702cd3312cb5ce
Sri Vignesh <sselvan@redhat.com>
2024-09-10 15:57:31 +0530

    fix: Increase timeout for tutorials test (#373)
    Signed-off-by: Sri Vignesh <sselvan@redhat.com>

--- steps/utility/utility.go
+++ steps/utility/utility.go
@@ -45,7 +45,7 @@ var _ = gauge.Step("Switch to project <projectName>", func(projectName string) {
 var _ = gauge.Step("Validate route url for pipelines tutorial", func() {
 	expectedOutput := "Cat 🐺 vs Dog 🐶"
 	routeUrl := store.GetScenarioData("routeurl")
-	output := cmd.MustSucceed("lynx", routeUrl, "--dump").Stdout()
+	output := cmd.MustSuccedIncreasedTimeout(30*time.Second, "lynx", routeUrl, "--dump").Stdout()
 	log.Println(output)
 	if !strings.Contains(output, expectedOutput) {
 		testsuit.T.Fail(fmt.Errorf("expected:\n%v,\ngot:\n%v", expectedOutput, output))
