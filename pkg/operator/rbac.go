0717b81d5ce6fe9db5d937a7648e616681cca5aa
OpenShift Cherrypick Robot <openshift-cherrypick-robot@redhat.com>
2025-09-05 12:43:20 +0200

    [release-v1.19] SRVKP-7749 Validate RBAC for ecosystem task (#664)
    Co-authored-by: jayesh-garg <jgarg@redhat.com>

--- pkg/operator/rbac.go
+++ pkg/operator/rbac.go
@@ -203,3 +203,17 @@ func AssertSCCNotPresent(clients *clients.Clients, sccName string) {
 		testsuit.T.Fail(fmt.Errorf("expected: security context constraint %q not present, Actual: security context constraint %q present, Error: %v", sccName, sccName, err))
 	}
 }
+
+func VerifyRolesArePresent(clients *clients.Clients, role, namespace string) {
+	err := wait.PollUntilContextTimeout(clients.Ctx, config.APIRetry, config.APITimeout, false, func(context.Context) (done bool, err error) {
+		log.Printf("Verifying that role %s exists in namespace %s\n", role, namespace)
+		_, err = clients.KubeClient.Kube.RbacV1().Roles(namespace).Get(context.TODO(), role, metav1.GetOptions{})
+		if err != nil {
+			return false, err
+		}
+		return true, nil
+	})
+	if err != nil {
+		testsuit.T.Fail(fmt.Errorf("failed to verify role %q present in namespace %q. Error: %v", role, namespace, err))
+	}
+}
