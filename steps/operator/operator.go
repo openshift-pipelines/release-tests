e7c9d7705d1bb3f60050bbb9b4d249d1cfc031a4
jayesh-garg <jgarg@redhat.com>
2025-09-05 15:54:48 +0530

    SRVKP-7749 Validate RBAC for ecosystem task (#657)

--- steps/operator/operator.go
+++ steps/operator/operator.go
@@ -3,8 +3,11 @@ package olm
 import (
 	"fmt"
 	"log"
+	"strings"
 
 	"github.com/getgauge-contrib/gauge-go/gauge"
+	"github.com/getgauge-contrib/gauge-go/models"
+	"github.com/getgauge-contrib/gauge-go/testsuit"
 	"github.com/openshift-pipelines/release-tests/pkg/cmd"
 	"github.com/openshift-pipelines/release-tests/pkg/operator"
 	"github.com/openshift-pipelines/release-tests/pkg/store"
@@ -22,4 +25,31 @@ var _ = gauge.Step("Verify RBAC resources disabled successfully", func() {
 
 var _ = gauge.Step("Verify RBAC resources are auto created successfully", func() {
 	operator.ValidateRBAC(store.Clients(), store.GetCRNames())
+})
+
+var _ = gauge.Step("Verify the roles are present in <namespace> namespace: <table>", func(namespace string, rolesTable *models.Table) {
+	gauge.GetScenarioStore()["rolesTable"] = rolesTable
+	for _, row := range rolesTable.Rows {
+		role := row.Cells[0]
+		operator.VerifyRolesArePresent(store.Clients(), role, namespace)
+	}
+})
+
+var _ = gauge.Step("Verify the total number of roles in <namespace> namespace matches the table", func(namespace string) {
+	rolesTable, ok := gauge.GetScenarioStore()["rolesTable"].(*models.Table)
+	if !ok {
+		testsuit.T.Errorf("Could not get rolesTable from scenario store")
+	}
+	fullOutput := cmd.MustSucceed("oc", "get", "role", "-n", namespace, "-o", "name").Stdout()
+	lines := strings.Split(fullOutput, "\n")
+	actualCount := 0
+	for _, line := range lines {
+		if strings.TrimSpace(line) != "" {
+			actualCount++
+		}
+	}
+	expectedCount := len(rolesTable.Rows)
+	if actualCount != expectedCount {
+		testsuit.T.Errorf("Mismatch in number of roles in namespace %s. Expected: %d (from table), Actual: %d (from oc get role)\nFull output of 'oc get role -n %s':\n%s", namespace, expectedCount, actualCount, namespace, fullOutput)
+	}
 })
