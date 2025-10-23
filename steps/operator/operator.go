ce912cd131f0cbfb9030c083775e98e449b83a74
Sai Raju Manthina <smanthin@redhat.com>
2025-10-23 13:04:00 +0530

    [release-v1.20] [SRVKP-8899] Added new test for CA Bundle ConfigMap creation control (#677)

--- steps/operator/operator.go
+++ steps/operator/operator.go
@@ -10,9 +10,9 @@ import (
 	"github.com/openshift-pipelines/release-tests/pkg/store"
 )
 
-var _ = gauge.Step("Update TektonConfig CR to use param with name createRbacResource and value <value> to <action> auto creation of RBAC resources", func(value, action string) {
-	patchData := fmt.Sprintf("{\"spec\":{\"params\":[{\"name\":\"createRbacResource\",\"value\":\"%s\"}]}}", value)
-	log.Println(action, "auto creation of RBAC resources")
+var _ = gauge.Step("Update TektonConfig CR to use param with name <paramName> and value <value> to <action> auto creation of <resourceType>", func(paramName, value, action, resourceType string) {
+	patchData := fmt.Sprintf("{\"spec\":{\"params\":[{\"name\":\"%s\",\"value\":\"%s\"}]}}", paramName, value)
+	log.Println(action, "auto creation of", resourceType)
 	log.Printf("output: %s\n", cmd.MustSucceed("oc", "patch", "TektonConfig", "config", "--type=merge", "-p", patchData).Stdout())
 })
 
@@ -24,8 +24,10 @@ var _ = gauge.Step("Verify RBAC resources are auto created successfully", func()
 	operator.ValidateRBAC(store.Clients(), store.GetCRNames())
 })
 
-// var _ = gauge.Step("Update TektonConfig CR to use param with name createCABundleConfigMaps and value <value> to <action> auto creation of CABundles", func(value, action string) {
-// 	patchData := fmt.Sprintf("{\"spec\":{\"params\":[{\"name\":\"createCABundleConfigMaps\",\"value\":\"%s\"}]}}", value)
-// 	log.Println(action, "auto creation of CABundle resources")
-// 	log.Printf("output: %s\n", cmd.MustSucceed("oc", "patch", "TektonConfig", "config", "--type=merge", "-p", patchData).Stdout())
-// })
+var _ = gauge.Step("Verify CA Bundle ConfigMaps are auto created successfully", func() {
+	operator.ValidateCABundleConfigMaps(store.Clients(), store.GetCRNames())
+})
+
+var _ = gauge.Step("Verify CA Bundle ConfigMaps still exist", func() {
+	operator.ValidateCABundleConfigMaps(store.Clients(), store.GetCRNames())
+})
